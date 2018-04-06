25bf5a7baaa323f994cd3af1ca100ddf1b9b6f90
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2018-04-06 12:59:49 +0100

    removed stuffs

---
+++ resources/components/cards/fixture.blade.php
@@ -0,0 +1,30 @@
+<div class="c-fixture-card">
+    <div class="c-fixture-card__top">
+        <span class="heading:very-small margin-bottom:very-small color:blue c-fixture-card__date">Sat 07 April</span>
+        <div class="c-fixture-card__competition">MCC University Match 2018</div>
+        <div class="c-fixture-card__venue">Lords</div>
+        <hr class="margin-y-axis:small border-color:light-grey">
+    </div>
+    <div class="c-fixture-card__middle">
+        <div class="children:same-height margin-bottom:small cf">
+            <div class="percent-col:twenty-five">
+                <img src="http://placehold.it/50x50" class="full-width" alt="">
+            </div>
+            <div class="percent-col:seventy-five child:vertically-aligned padding-left:small">
+                <div>Middlesex</div>
+            </div>
+        </div>
+        <div class="children:same-height cf">
+            <div class="percent-col:twenty-five">
+                <img src="http://placehold.it/50x50" class="full-width" alt="">
+            </div>
+            <div class="percent-col:seventy-five child:vertically-aligned padding-left:small">
+                <div>Northamptonshire</div>
+            </div>
+        </div>
+    </div>
+    <div class="c-fixture-card__bottom">
+        <hr class="margin-y-axis:small">
+        <a href="/" class="button:small:black full-width margin-bottom:very-small">Match Center</a>
+    </div>
+</div>
