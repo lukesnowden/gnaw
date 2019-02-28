2aacad6bb1edfd3aa011cff16b14642cc073fef5
Luke Snowden <luke@purposemedia.co.uk>
2019-02-28 10:45:38 +0000

    added full width helper

--- app/Generators/Utilities/Helpers.php
+++ app/Generators/Utilities/Helpers.php
@@ -78,6 +78,7 @@ class Helpers extends SelectorGenerator implements Generator
 
             $content .= ".{$prefix}font-style\:italic { font-style:italic; }\n";
             $content .= ".{$prefix}text-decoration\:underline { text-decoration:underline; }\n";
+            $content .= ".{$prefix}full-width\:underline { width:100%; }\n";
 
             if( $prefix ) {
                 $content .= "}\n";
